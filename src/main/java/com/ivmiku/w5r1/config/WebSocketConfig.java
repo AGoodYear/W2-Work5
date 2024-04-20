478edb895e0e078ec7141ffbf493e4969c658f29
ivmiku <124345843+ivmiku@users.noreply.github.com>
2024-04-20 13:24:45 +0800

    fix:image send issue

--- src/main/java/com/ivmiku/w5r1/config/WebSocketConfig.java
+++ src/main/java/com/ivmiku/w5r1/config/WebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 import org.springframework.web.util.WebAppRootListener;
 
 @Configuration
@@ -19,5 +20,15 @@ public class WebSocketConfig implements ServletContextInitializer {
     public void onStartup(ServletContext servletContext) throws ServletException {
         servletContext.addListener(WebAppRootListener.class);
         servletContext.setInitParameter("org.apache.tomcat.websocket.textBufferSize","1024000");
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        // 在此处设置bufferSize
+        container.setMaxTextMessageBufferSize(50*1024*1024);
+        container.setMaxBinaryMessageBufferSize(50*1024*1024);
+        container.setMaxSessionIdleTimeout(15 * 60000L);
+        return container;
     }
 }
